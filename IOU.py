2cc84ab8017f547d82cbeb608b6d2d822e5f5e23
Arvind s <37202614+avind1096@users.noreply.github.com>
2018-12-30 20:57:46 +0530

    Implement the intersection over union (IoU) between box1 and box2

---
+++ IOU.py
@@ -0,0 +1,27 @@
+def iou(box1, box2):
+    """
+
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. 
+    
+    xi1 = max(box1[0], box2[0])
+    yi1 = max(box1[1], box2[1])
+    xi2 = min(box1[2], box2[2])
+    yi2 = min(box1[3], box2[3])
+    inter_area = (xi2 - xi1) * (yi2 - yi1)
+      
+
+    # Calculating the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    union_area = box1_area + box2_area - inter_area
+
+    # compute the IoU
+    iou = inter_area / union_area
+
+    return iou
